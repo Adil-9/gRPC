7cf492724e2a1b4c93a78d5b5da32b054d597290
adil <student@ALEM-F3-L06-02.alem.school>
2024-01-26 16:45:41 +0600

    not empty

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,9 +1,19 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"grpc/internal/config"
+)
 
 // for sso start up
 func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	//	todo init object of the config
 	//	todo init logger
 	// 	todo init the applicaiton (the intry point)
 	//	todo start grpc server of the app
+}
+
+func setupLogger(enc string) *slog.Logger {
 }
--- sso/internal/config/config.go
+++ sso/internal/config/config.go
@@ -1,7 +1,51 @@
 package config
 
-type config struct {
-	Env          string `yaml:"env" env-default:"local"`
-	Storage_path string `yaml:"storage_path"`
-	Grpc
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env          string        `yaml:"env" env-default:"local"`
+	Storage_path string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL     time.Duration `yaml:"token_ttl" env-requred:"true"`
+	GRPC         GRPCCOnfig    `yaml:"grpc"`
+}
+
+type GRPCCOnfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exits: " + path)
+	}
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
 }
